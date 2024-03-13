67dc288610a0ebd02222a04228465f7dbe07bd49
Elisabeth FAUJOUR <efaujour@gmail.com>
2024-03-13 19:02:41 +0100

    :recycle: Refacto Career Section

--- src/components/career/Career.tsx
+++ src/components/career/Career.tsx
@@ -1,3 +1,4 @@
+import { careersSection } from '@/data/data';
 import FadeInDiv from '../animations/FadeInDiv/FadeInDiv';
 import FadeInSection from '../animations/FadeInSection/FadeInSection';
 import { BriefcaseFill } from 'react-bootstrap-icons';
@@ -11,134 +12,41 @@ function Career() {
     <FadeInSection customClass={`${styles.career} main-layout`} id="career">
       <h2 className={`${styles.title} main-title`}>Mon parcours.</h2>
       <div className={styles.timeline}>
-        <FadeInDiv customClass={styles.container}>
-          <div className={`${styles.icon} ${styles.left}`}>
-            <BriefcaseFill size={20} color="#0f0b25" />
-          </div>
-          <div className={styles.block}>
-            <h3 className={styles.title}>
-              Développeur Web Front-End - Projet de fin de formation
-            </h3>
-            <p className={styles.date}>
-              <Calendar3 /> Septembre 2023 – octobre 2023
-            </p>
-            <h4 className={styles.subtitle}>
-              O&apos;Journey - Application d&apos;organisation de voyage
-              interactive
-            </h4>
-            <ul className={styles.list}>
-              <li className={styles.elem}>
-                Projet réalisé en 1 mois, en équipe de 5 développeurs et
-                reposant sur les méthodologie SCRUM et AGILE.
-              </li>
-              <li className={styles.elem}>
-                Technos : React, Redux, Typescript, SCSS, API REST.
-              </li>
-              <li className={styles.elem}>
-                Compétences : MVC, SPA, Responsive Design, UX/UI,
-                Authentification par JWT, API REST, axios, Vercel, GitFlow, SEO,
-                accessibilité, recommandations OWASP.
-              </li>
-            </ul>
-            <p className={styles.location}>
-              <BuildingFill /> Ecole O&apos;Clock (certifiée grande école du
-              numérique)
-            </p>
-          </div>
-        </FadeInDiv>
-
-        <FadeInDiv customClass={styles.container}>
-          <div className={`${styles.icon} ${styles.right}`}>
-            <MortarboardFill size={20} color="#0f0b25" />
-          </div>
-          <div className={styles.block}>
-            <h3 className={styles.title}>
-              Titre RNCP Niveau 5 &apos;Développeur Web et Web Mobile&apos;
-            </h3>
-            <p className={styles.date}>
-              <Calendar3 /> Mai – Octobre 2023
-            </p>
-            <h4 className={styles.subtitle}>Formation intensive de 798h</h4>
-            <ul className={styles.list}>
-              <li className={styles.elem}>3 mois de socle de Compétences</li>
-              <li className={styles.elem}>1 mois de Spécialisation React</li>
-              <li className={styles.elem}>
-                1 mois de projet professionnel en équipe
-              </li>
-            </ul>
-            <p className={styles.location}>
-              <BuildingFill /> Ecole O&apos;Clock (certifiée grande école du
-              numérique)
-            </p>
-          </div>
-        </FadeInDiv>
-
-        <FadeInDiv customClass={styles.container}>
-          <div className={`${styles.icon} ${styles.left}`}>
-            <BriefcaseFill size={20} color="#0f0b25" />
-          </div>
-          <div className={styles.block}>
-            <h3 className={styles.title}>Masseur-kinésithérapeute libéral</h3>
-            <p className={styles.date}>
-              <Calendar3 /> Juillet 2016 – avril 2023
-            </p>
-            <ul className={styles.list}>
-              <li className={styles.elem}>
-                Remplaçante à l&apos;Ile de la Réunion pendant 6 mois
-                post-diplôme
-              </li>
-              <li className={styles.elem}>
-                Assitante-collaboratrice en cabinet de groupe avec une activité
-                mixte cabinet/domicile dans la région Grenobloise.
-              </li>
-              <li className={styles.elem}>
-                Spécialisation en Méthode Mckenzie de Diagnostic et Thérapie
-                Mécanique{' '}
-              </li>
-              <li className={styles.elem}>
-                Maîtrise des logiciels de gestion de cabinet et de
-                télétransmission : VEGA, Topaze, Kiné+4000, Doctolib{' '}
-              </li>
-              <li className={styles.elem}>
-                Tenue de comptabilité professionelle.
-              </li>
-            </ul>
-          </div>
-        </FadeInDiv>
-
-        <FadeInDiv customClass={styles.container}>
-          <div className={`${styles.icon} ${styles.right}`}>
-            <MortarboardFill size={20} color="#0f0b25" />
-          </div>
-          <div className={styles.block}>
-            <h3 className={styles.title}>
-              Diplôme d&apos;état de Masseur-kinésithérapeute
-            </h3>
-            <p className={styles.date}>
-              <Calendar3 /> 2013 – 2016
-            </p>
-            <p className={styles.location}>
-              <BuildingFill /> Ecole de kinésithérapie du CHU de Grenoble
-            </p>
-          </div>
-        </FadeInDiv>
-
-        <FadeInDiv customClass={styles.container}>
-          <div className={`${styles.icon} ${styles.left}`}>
-            <MortarboardFill size={20} color="#0f0b25" />
-          </div>
-          <div className={styles.block}>
-            <h3 className={styles.title}>
-              PACES (Première Année Commune Aux études de Santé)
-            </h3>
-            <p className={styles.date}>
-              <Calendar3 /> 2011 – 2013
-            </p>
-            <p className={styles.location}>
-              <BuildingFill /> Université Grenobles Alpes - UFR Médecine
-            </p>
-          </div>
-        </FadeInDiv>
+        {careersSection.map((career, index) => (
+          <FadeInDiv key={index} customClass={styles.container}>
+            <div
+              className={`${styles.icon} ${index % 2 === 0 ? styles.left : styles.right}`}
+            >
+              {index % 2 === 0 ? (
+                <BriefcaseFill size={20} color="#0f0b25" />
+              ) : (
+                <MortarboardFill size={20} color="#0f0b25" />
+              )}
+            </div>
+            <div className={styles.block}>
+              <h3 className={styles.title}>{career.title}</h3>
+              <p className={styles.date}>
+                <Calendar3 /> {career.date}
+              </p>
+              {career.subtitle && (
+                <h4 className={styles.subtitle}>{career.subtitle}</h4>
+              )}
+              <ul className={styles.list}>
+                {career.description &&
+                  career.description.map((desc, idx) => (
+                    <li key={idx} className={styles.elem}>
+                      {desc}
+                    </li>
+                  ))}
+              </ul>
+              {career.location && (
+                <p className={styles.location}>
+                  <BuildingFill /> {career.location}
+                </p>
+              )}
+            </div>
+          </FadeInDiv>
+        ))}
       </div>
     </FadeInSection>
   );
